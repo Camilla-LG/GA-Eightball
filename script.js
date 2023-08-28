5e4518d05e434714dded6afcf19f1798bca1f53c
camillalo1 <camillalo@getacademy.no>
2023-08-28 22:48:59 +0200

    gradient color

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function updateView(){
     document.getElementById("eightball").innerHTML = /*HTML*/ `
 
     <h1> Magic 8ball </h1>
-    
+
     <div class = "window">
         <div class = "ball8">
             <div class = "whiteCircle" onclick ="getRandomAnswer()">
@@ -27,7 +27,7 @@ function updateView(){
 
 function getRandomAnswer(){
     let randomIndex = Math.floor(Math.random() * cueBall.length);
-    let cueBallTxt = cueBall.length > 0 ? cueBall[randomIndex] : 'Cash, please'
+    let cueBallTxt = cueBall.length > 0 ? cueBall[randomIndex] : 'Go Away!'
     classFont = '';
     startPoint = cueBallTxt;
     cueBall.splice(randomIndex, 1);
