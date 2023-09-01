3a8484d38ecb915b65c59cbb693041b7f35dbd2d
camillalo1 <camillalo@getacademy.no>
2023-09-01 14:20:52 +0200

    input draft

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let cueBall = ['YES!', 
+let yesNoBall = ['YES!', 
             'Try Again', 
             'No', 
             'Absolutely Not', 
             'Definitely Yes'];
+let timeBall = ['Soon',
+            'Never',
+            'Always',
+            'Tomorrow',
+            'Next Week',
+            'Next Month',
+            'Next Year']
+let numberBall = ['5',
+            'Just a Few',
+            '3',
+            '10',
+            'Many More']
 let startPoint = '8';
 let classFont = 'font';
 
@@ -13,6 +25,7 @@ function updateView(){
 
     <h1> Magic 8ball </h1>
 
+    <div class = "main">
     <div class = "window">
         <div class = "ball8">
             <div class = "whiteCircle" onclick ="getRandomAnswer()">
@@ -22,14 +35,20 @@ function updateView(){
             </div>
         </div>
     </div>
+    <div class = "inputArea">
+        <h4> Ask me Anything </h4>
+        <br/>
+        <input type = "text" id = "userInput" onchange = "">
+    </div>
+    </div>
     ;`
 }
 
 function getRandomAnswer(){
-    let randomIndex = Math.floor(Math.random() * cueBall.length);
-    let cueBallTxt = cueBall.length > 0 ? cueBall[randomIndex] : 'Go Away!'
+    let randomIndex = Math.floor(Math.random() * yesNoBall.length);
+    let yesNoBallTxt = yesNoBall.length > 0 ? yesNoBall[randomIndex] : 'Go Away!'
     classFont = '';
-    startPoint = cueBallTxt;
-    cueBall.splice(randomIndex, 1);
+    startPoint = yesNoBallTxt;
+    // yesNoBall.splice(randomIndex, 1);
     updateView();
 }
